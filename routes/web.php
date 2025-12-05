817dc778a2cdbae47fbf9cd2e03a76d732a6b80f
akmalfman <akmalfsalman2@gmail.com>
2025-12-05 15:10:42 +0700

    Model preparation

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    sleep(5);
-    return Inertia::render('Home', ['name' => 'Eko']);
-});
+
+Route::get('/', [PostController::class, 'index']);
+Route::resource('posts', PostController::class)->except('index');
+
+
+// Route::get('/', function () {
+//     sleep(5);
+//     return Inertia::render('Home', ['name' => 'Eko']);
+// });
 
 // Route::inertia('/', 'Home');
